ca60947c7c229b904225e20c03d0bcb623988186
ANTHONY LIMA E SILVA <72108893+Anthony-a-l-s@users.noreply.github.com>
2023-12-20 11:25:34 -0300

    mods

--- app/publisher.py
+++ app/publisher.py
@@ -8,26 +8,23 @@ import json
 
 coletores = []
 for i in range(4):
-    coletores.append(Coletor(i, 5, 1))
+    coletores.append(Coletor(i, 5))
 
 client = paho.Client()
 
-if client.connect("192.168.100.14", 1883, 60) != 0:
+if client.connect("10.5.16.131", 1890, 60) != 0:
     print("Couldn't connect to the mqtt broker")
     sys.exit(1)
 
-ticks = 0
+
 # dados = []
 while (True):
-    if ticks == 30:
-        break
-    else:
-        ticks = ticks + 1
-        time.sleep(2)
-        for coletor in coletores:
-            msg = json.loads(coletor.coletar_dados())
-            # dados.append(st.mean(coletor.coletar_dados()))
-            # m_temp = str(st.mean(dados))
-            client.publish("temperature_topic", str(msg), 0)
+
+    time.sleep(2)
+    for coletor in coletores:
+        msg = json.loads(coletor.coletar_dados())
+        # dados.append(st.mean(coletor.coletar_dados()))
+        # m_temp = str(st.mean(dados))
+        client.publish("temperature_topic", str(msg), 0)
 
 client.disconnect()
